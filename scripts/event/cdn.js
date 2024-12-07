d3add57c1b583be3af5d4e6b4388b59f1ac458d5
Bai He <baihexx@gmail.com>
2024-12-07 10:23:11 +0800

    去除不必要的post_ai.min.js文件引入

--- scripts/event/cdn.js
+++ scripts/event/cdn.js
@@ -64,11 +64,6 @@ hexo.extend.filter.register('before_generate', () => {
             file: 'js/tw_cn.js',
             version
         },
-        post_ai: {
-            name: 'hexo-theme-solitude',
-            file: 'js/third_party/post_ai.min.js',
-            version
-        },
         commentBarrage: {
             name: 'hexo-theme-solitude',
             file: 'js/third_party/barrage.min.js',
